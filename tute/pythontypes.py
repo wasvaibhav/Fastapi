174fbfd7adf40d070d579e0b010c864e8363375b
wasvaibhav <113747620+wasvaibhav@users.noreply.github.com>
2026-07-05 09:19:28 +0530

    update

---
+++ tute/pythontypes.py
@@ -0,0 +1,18 @@
+# def add(firstName: str |None ,lastName: str = None):
+#     if(firstName != None):
+#         firstName.capatalize()
+    
+#     return firstName + " " +lastName
+
+# fname = None
+# lname = "Gates"
+
+# name = add(fname , lname)
+# print(name):
+
+
+class Person:
+    def__init__(self,name: str )
+    self.name = name 
+def get_person_name(one_person: person):
+    return one_person.self
